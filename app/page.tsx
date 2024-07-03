ecbc696756ce70db62c0cf0ddf4e79fb62bd8344
lawrence-mutuku <malelulawrence@gmail.com>
2024-07-03 22:28:42 +0300

    edit page.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import Hero from "@/components/Hero";
 import GridBackground from "@/components/ui/GridBackground";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="dark w-screen h-screen flex justify-center items-center">
-      {/* Todo just placeholder */}
-     <GridBackground />
+    <main className="w-screen text-[#1048a0]">
+      <div className="h-screen">
+        <Hero />
+      </div>
+      
     </main>
   );
 }
